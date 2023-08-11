bd382d9bd1a77d231671508b8132c6914fb5c2d2
Jon Cummings <jon@cummings.live>
2023-08-11 14:23:33 -0700

    Add startup msg

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 
 	server.AddContentUpdater(&server.TradingJournalUpdater{Directory: *historyFilePathPtr, Server: &server.TradeServer{}}, updateStream)
 
+	fmt.Printf("Starting TradeWiz Server\n")
+
 	srv := &http.Server{Addr: fmt.Sprintf(":%d", 37777)}
 	srv.ListenAndServe()
 }
